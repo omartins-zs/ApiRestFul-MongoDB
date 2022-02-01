2a5f2b3759cd052425b395a90038f59ae0c34760
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-01 08:51:15 -0300

    Criando arquivo de Routes

---
+++ routes/personRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+
+// Rotas da Api 
+router.post('/person', async (req, res) => {
+
+    // req.body
+
+    // {name: "Gabriel", salary: 600, approved: false}
+    const { name, salary, approved } = req.body
+
+    if (!name) {
+        res.status(422).json({ message: 'O nome é obrigatório!' })
+    }
+
+    const person = {
+        name,
+        salary,
+        approved
+    }
+
+    // Create 
+    try {
+        await Person.create(person)
+
+        res.status(201).json({ message: 'Pessoa inserida no sistema com sucesso!!' })
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
+module.exports = router;
